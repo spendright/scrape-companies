d6f36a3d26774be675c116b0ba7485b55f4743e2
David Marin <dm@davidmarin.org>
2014-05-25 19:58:50 -0700

    added TODO about 403 for Unilever, removed unnecessary R_AND_TM

--- scrapers/unilever.py
+++ scrapers/unilever.py
@@ -3,16 +3,17 @@ from bs4 import BeautifulSoup
 import scraperwiki
 
 
+# TODO: Get around 403 Forbidden. Send more browser-like headers?
+# TODO: scrape all countries, not just U.S.
+
 COMPANY = 'Unilever'
 
 # this page says Unilever has "more than 1000 brands" (!)
 TOP_BRANDS_URL = 'http://www.unileverusa.com/brands-in-action/view-brands.aspx?view=AtoZ'
 
 # Best Foods is an alternate name for Hellmann's (mayo)
-# Marmite and PG Tips are UK brands available in the US
+# Marmite and PG Tips are UK brands available in the U.S.
 EXTRA_BRANDS = ['Best Foods', 'Marmite', 'PG Tips']
-
-R_AND_TM = u'®™'
 
 
 def scrape_brands():
@@ -23,12 +24,4 @@ def scrape_brands():
     soup = BeautifulSoup(scraperwiki.scrape(TOP_BRANDS_URL))
 
     for span in soup.select('.zlist span.title'):
-        brand = span.text
-
-        for c in R_AND_TM:
-            if c in brand:
-                # cut off everything after the ®/™
-                # e.g. Consort® For Men becomes simply "Consort"
-                brand = brand[:brand.index(c)]
-
-        yield brand
+        yield span.text
