ac8e22ae08b058fb1b9c4c2484688c8881f30b12
David Marin <dm@davidmarin.org>
2014-09-07 17:12:21 -0700

    update unilever scraper to include categories

--- scrapers/unilever.py
+++ scrapers/unilever.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import json
+
 from urllib2 import Request
 from urllib2 import urlopen
 
@@ -6,6 +8,7 @@ from bs4 import BeautifulSoup
 
 
 # TODO: scrape all countries, not just U.S.
+# will probably want to use U.S. category names (match on color)
 
 COMPANY = 'Unilever'
 
@@ -18,19 +21,33 @@ HTTP_HEADERS = {
     'User-Agent': 'Mozilla/5.0',
 }
 
-
 # Best Foods is an alternate name for Hellmann's (mayo)
-# Marmite and PG Tips are UK brands available in the U.S.
-EXTRA_BRANDS = ['Best Foods', 'Marmite', 'PG Tips']
+# Marmite and PG Tips are U.K. brands available in the U.S.
+#
+# TODO: just scrape from the U.K. page, and let people make a reasonable
+# guess that U.K. brands might be available from U.S. websites.
+EXTRA_BRANDS = {
+    'Best Foods': 'Food and drink',
+    'Marmite': 'Food and drink',
+    'PG Tips': 'Food and drink',
+}
 
 
 def scrape_brands():
     yield COMPANY
-    for brand in EXTRA_BRANDS:
-        yield brand
+    for brand, category in sorted(EXTRA_BRANDS.items()):
+        yield {'brand': brand, 'category': category}
 
     html = urlopen(Request(URL, headers=HTTP_HEADERS)).read()
     soup = BeautifulSoup(html)
 
-    for span in soup.select('.zlist span.title'):
-        yield span.text
+    key_to_cat = dict((a['data-filter'], a.text)
+                      for a in soup.select('div.boxSellcontent a'))
+
+    for div in soup.select('.zlist'):
+        cat_keys = json.loads(div['data-category'])['c']
+        categories = [key_to_cat[k] for k in cat_keys]
+
+        brand = div.find('span', class_='title').text
+
+        yield {'brand': brand, 'category': category}
