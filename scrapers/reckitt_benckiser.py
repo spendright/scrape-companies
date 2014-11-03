d589c908277a9fa89af02d931ef5396d13e8719d
David Marin <dm@davidmarin.org>
2014-11-02 21:29:16 -0800

    added categories. still need to better match logos with brands

--- scrapers/reckitt_benckiser.py
+++ scrapers/reckitt_benckiser.py
@@ -1,14 +1,15 @@
-from urlparse import urljoin
-
 from srs.scrape import scrape_soup
 
 
 
 COMPANY = u'Reckitt-Benckiser'
 
+COMPANY_URL = 'http://www.rb.com/'
 
-# TODO: this no longer exists. Use #scroller from
 BRANDS_URL = 'http://www.rb.com/our-brands'
+
+CATEGORY_URL = 'http://www.rb.com/investors-media/category-performance'
+
 
 MORE_BRANDS = [
     COMPANY,
@@ -17,16 +18,30 @@ MORE_BRANDS = [
 ]
 
 
-def scrape_brands():
+def scrape_company():
+
+    yield 'company', dict(company=COMPANY, url=COMPANY_URL)
 
     for brand in MORE_BRANDS:
-        yield brand
+        yield 'brand', dict(company=COMPANY, brand=brand)
 
     brands_soup = scrape_soup(BRANDS_URL)
 
     for img in brands_soup.select('#scroller img'):
-        yield dict(brand=img['alt'],
-                   logo_url=urljoin(BRANDS_URL, img['src']))
+        yield 'brand', dict(company=COMPANY,
+                            brand=img['alt'],
+                            logo_url=img['src'])
 
-        # TODO: use http://www.rb.com/media-investors/category-performance
-        # to get brand categories
+    cat_soup = scrape_soup(CATEGORY_URL)
+
+    for a in cat_soup.select('li.active ul li a'):
+        cat = a.text
+        url = a['href']
+
+        soup = scrape_soup(url)
+        for h2 in soup.select('h2'):
+            brand = h2.text.strip()
+            if brand:
+                yield 'brand', dict(company=COMPANY,
+                                    brand=h2.text,
+                                    category=cat)
