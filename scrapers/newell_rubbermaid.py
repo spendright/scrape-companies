87a0acd2723a623dfc1e73871a3b9f6d6798be3e
David Marin <dm@davidmarin.org>
2014-11-02 21:03:46 -0800

    fixed newell_rubbermaid scraper (bad HTML comment)

--- scrapers/newell_rubbermaid.py
+++ scrapers/newell_rubbermaid.py
@@ -1,15 +1,21 @@
 from urlparse import urljoin
 
-from srs.scrape import scrape_soup
+from bs4 import BeautifulSoup
 
+from srs.scrape import scrape
 
 
 COMPANY = 'Newell Rubbermaid'
 START_URL = 'http://www.newellrubbermaid.com/OurBrands/Pages/AllBrands.aspx'
 
 
+def scrape_soup_from_bad_html(url):
+    """Discard a bad comment that's frustrating BeautifulSoup"""
+    return BeautifulSoup(scrape(url)[3:])
+
+
 def scrape_brands():
-    start_soup = scrape_soup(START_URL)
+    start_soup = scrape_soup_from_bad_html(START_URL)
 
     # if page structure changes, we'll get errors
     nav = start_soup.find('div', class_='switchNav1')
@@ -20,7 +26,7 @@ def scrape_brands():
             continue
 
         category = a.text
-        soup = scrape_soup(url)
+        soup = scrape_soup_from_bad_html(url)
 
         for brand_a in soup.select('.switchNav1 ul li ul li a'):
             yield {'brand': brand_a.text, 'category': category}
