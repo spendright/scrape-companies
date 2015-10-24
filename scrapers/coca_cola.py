ae7939a4328fb8f1bcf6b952b79a1bb07ce5259e
David Marin <dm@davidmarin.org>
2015-10-24 12:13:24 -0700

    fixed coca_cola scraper

--- scrapers/coca_cola.py
+++ scrapers/coca_cola.py
@@ -13,6 +13,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+from urlparse import urljoin
 from urllib2 import urlopen
 import json
 
@@ -20,24 +21,39 @@ from bs4 import BeautifulSoup
 
 COMPANY = 'Coca-Cola Company'
 
+COMPANY_URL = 'http://www.coca-colacompany.com/'
+
 # Coca-Cola brands are a major source of false positives and not that
 # important to searching on Amazon, so sticking to US brands for now.
 #
 # TODO: query each country and keep track of which countries brands are in.
 #
 # This returns JSON for an AJAX script.
-# The corresponding page is http://www.coca-colacompany.com/brands/all/
-US_BRANDS_URL = 'http://www.coca-colacompany.com/api/getAllBrands?country=united-states'
+
+ALL_BRANDS_URL = 'http://www.coca-colacompany.com/brands/all/'
+
+# TODO: fetch brands for each country
+US_BRANDS_JSON_URL = 'http://www.coca-colacompany.com/brands/all/_jcr_content/pageContent/allbrandslist.Ajax.json/us.json'
 
 
-def scrape_brands():
+def scrape_company():
 
-    brand_json = json.load(urlopen(US_BRANDS_URL))
+    yield 'company', {'company': COMPANY, 'url': COMPANY_URL}
 
-    for brand in brand_json['brands']:
-        name_html = brand['name']['desktop']
+    brand_json = json.load(urlopen(US_BRANDS_JSON_URL))
+
+    for brand_dict in brand_json['brands']:
+        brand = dict(company=COMPANY)
+
+        name_html = brand_dict['name']['desktop']
         name = BeautifulSoup(name_html).text.strip()  # resolve HTML entities
         if name.endswith('*'):
-            yield dict(brand=name[:-1], is_licensed=True)
+            brand['brand'] = name[:-1]
+            brand['is_licensed'] = True
         else:
-            yield name
+            brand['brand'] = name
+
+        if brand_dict['brand_url']:
+            brand['url'] = urljoin(ALL_BRANDS_URL, brand_dict['brand_url'])
+
+        yield 'brand', brand
